478a00e12770f605ca295d5bb91d279531d6a638
Surya Sankar <suryashankar.m@gmail.com>
2018-12-17 12:40:59 +0530

    modified the blog theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -31,5 +31,7 @@ SOCIAL = (('You can add links in your config file', '#'),
 
 DEFAULT_PAGINATION = 10
 
+THEME = "/home/surya/techonometrics/pelican-themes/medius"
+
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
